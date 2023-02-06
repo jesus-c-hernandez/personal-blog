f4d14f622be7807189605dc767ed0b6e920e8c47
jesusSaul <jessiehdez36@gmail.com>
2023-02-05 17:32:43 -0700

    Build header responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Modules
+import { ComponentsModule } from './components/components.module';
+
+// Angular Material
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // Modules
+    ComponentsModule,
+    // Angular Material
   ],
   providers: [],
   bootstrap: [AppComponent]
